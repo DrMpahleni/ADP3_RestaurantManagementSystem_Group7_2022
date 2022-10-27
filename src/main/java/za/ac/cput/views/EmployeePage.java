ce0cbbec51f79af96cd0c50d145843bd08787585
Vuyisa <218192215@mycput.ac.za>
2022-10-27 19:52:23 +0200

    2nd commit

--- src/main/java/za/ac/cput/views/EmployeePage.java
+++ src/main/java/za/ac/cput/views/EmployeePage.java
@@ -19,12 +19,13 @@ public class EmployeePage {
     DefaultTableModel mdlTable;
     JScrollPane scrollEmployee;
     JLabel lblHead;
-    JButton btnEmployees, btnBack;
+    JButton btnEmployees, btnBack, btnAdd;
     Font ftHead;
-    String[] columnNames = {"Employee Id", "First Name", "Last Name", "Gender", "Employee Race","Contact", "Contact Type","Job",
+
+    String[] columnNames = {"Employee Id", "First Name", "Last Name", "Gender", "Employee Race", "Contact", "Contact Type", "Job",
             "Job Description", "Position"};
-    Object[][] tblData = {{null, null}};
-    Object[] column = {"Employee Id", "First Name", "Last Name", "Gender", "Employee Race","Contact", "Contact Type","Job",
+    Object[][] tblData = {{null, null, null, null, null, null, null, null, null, null}};
+    Object[] column = {"Employee Id", "First Name", "Last Name", "Gender", "Employee Race", "Contact", "Contact Type", "Job",
             "Job Description", "Position"};
 
     public EmployeePage() {
@@ -38,15 +39,18 @@ public class EmployeePage {
         lblHead = new JLabel("Staff List");
         btnEmployees = new JButton("Employees");
         btnBack = new JButton("Return");
+        btnAdd = new JButton("Add");
         ftHead = new Font(Font.SANS_SERIF, Font.BOLD, 25);
 
         lblHead.setFont(ftHead);
         tblEmployee.setFillsViewportHeight(true);
+        scrollEmployee.setPreferredSize(new Dimension(900, 600));
+
 
         TableColumn tblColumn = null;
         for (int i = 0; i < 10; i++) {
             tblColumn = tblEmployee.getColumnModel().getColumn(i);
-            tblColumn.setPreferredWidth(140);
+            tblColumn.setPreferredWidth(200);
         }
 
         this.setGui();
@@ -57,18 +61,24 @@ public class EmployeePage {
         frame.add(BorderLayout.CENTER, pnlCenter);
         frame.add(BorderLayout.SOUTH, pnlSouth);
 
+        pnlNorth.setBackground(new Color(0, 106, 255));
+        pnlCenter.setBackground(new Color(0, 106, 255));
+        pnlSouth.setBackground(new Color(0, 106, 255));
+
         pnlNorth.add(lblHead);
 
         pnlCenter.add(tblEmployee.getTableHeader(), BorderLayout.PAGE_START);
-        pnlCenter.add(scrollEmployee,BorderLayout.CENTER);
+        pnlCenter.add(scrollEmployee, BorderLayout.CENTER);
 
+        pnlSouth.add(btnAdd);
         pnlSouth.add(btnEmployees);
         pnlSouth.add(btnBack);
+
 
         btnEmployees.addActionListener(buttonActions());
 
         frame.setTitle("Employee List");
-        frame.setSize(1600, 800);
+        frame.setSize(1000, 800);
         frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
         frame.setVisible(true);
 
@@ -83,25 +93,31 @@ public class EmployeePage {
                 mdlTable.setColumnIdentifiers(column);
                 tblEmployee.setModel(mdlTable);
 
-                String url = "jdbc:mysql://localhost:3306/rms";
+                String url = "jdbc:mysql://localhost:3306/thechowloungedatabase";
                 String user = "root";
                 String pass = "password";
 
                 try {
-                    java.sql.Connection myConn = DriverManager.getConnection(url,user, pass);
+                    java.sql.Connection myConn = DriverManager.getConnection(url, user, pass);
                     Statement mySat = myConn.createStatement();
                     String sql = "SELECT * FROM users";
                     ResultSet rs = mySat.executeQuery(sql);
 
-                    while(rs.next()) {
-                        Object d[] = {rs.getString("Employee Id"), rs.getString("First Name"),rs.getString("Last Name"),
-                                rs.getString("Gender"),rs.getString("Employee Race"),rs.getString("Contact"), rs.getString("Contact Type"),
-                        rs.getString("Job"), rs.getString("Job Description"),rs.getString("Position")};
+                    while (rs.next()) {
+                        Object d[] = {rs.getString("Employee Id"),
+                                rs.getString("First Name"),
+                                rs.getString("Last Name"),
+                                rs.getString("Gender"),
+                                rs.getString("Employee Race"),
+                                rs.getString("Contact"),
+                                rs.getString("Contact Type"),
+                                rs.getString("Job"),
+                                rs.getString("Job Description"),
+                                rs.getString("Position")};
                         mdlTable.addRow(d);
                     }
 
-                }
-                catch (Exception ex) {
+                } catch (Exception ex) {
                     System.out.println(ex);
                 }
             }
@@ -111,12 +127,20 @@ public class EmployeePage {
             @Override
             public void actionPerformed(ActionEvent e) {
 
+                new DashboardPage();
                 frame.dispose();
             }
         });
 
+    btnAdd.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            new AddEmployee();
+            frame.dispose();
+        }
+    });
         return null;
-    }
+}
 
     public static void main(String[] args) {
         new EmployeePage();
